2a742fbde5ad1db7f6dd23487b57f7accb11cc7f
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2023-03-28 13:43:39 -0500

    QOL changes ChimeraPyOrchestrator;Remote Execution;Formatting
    1. Tile rendered outputs from YOLO to use a single show window
    2. Use utilities from chimerapy orchestrator for executing the pipeline
    3. Minor changes to formatting/constructors for ease of use
    4. Add configuration for execution of pipeline

--- mmlapipe/yolo_node.py
+++ mmlapipe/yolo_node.py
@@ -1,9 +1,10 @@
 from typing import Dict, List, Literal, Optional
 
 import chimerapy as cp
-import numpy as np
 import cv2
 import imutils
+import numpy as np
+from chimerapy_orchestrator.utils import register_chimerapy_node
 
 # Reference: https://tech.amikelive.com/node-718/what-object-categories-labels-are-in-coco-dataset/
 COCO_ORIGINAL_NAMES = [
@@ -89,34 +90,42 @@ COCO_ORIGINAL_NAMES = [
     "toothbrush",
 ]
 
-class YOLONode(cp.Node):
 
-    def __init__(self, name:str, classes:Optional[List[str]]=None, debug:Literal['step', 'stream']=None):
+@register_chimerapy_node
+class YOLONode(cp.Node):
+    def __init__(
+        self,
+        name: str,
+        classes: Optional[List[str]] = None,
+        per_row_display=2,
+        debug: Literal["step", "stream"] = None,
+    ):
         # Obtain the index of the object in the original list of classes
         self.interested_classes_idx = []
         if classes:
-            for obj_class in classes: 
+            for obj_class in classes:
                 class_index = COCO_ORIGINAL_NAMES.index(obj_class)
                 self.interested_classes_idx.append(class_index)
-        
+
+        self.per_row_display = per_row_display
+
         super().__init__(name=name, debug=debug)
 
     def prep(self):
-
         # Create the YOLOv5 model
         import torch
+
         self.model = torch.hub.load("ultralytics/yolov5", "yolov5s", trust_repo=True)
-        
+
         # Select only the interested classes
         if self.interested_classes_idx:
             self.model.classes = self.interested_classes_idx
 
     def step(self, data_chunks: Dict[str, cp.DataChunk]):
-
         # Aggreate all inputs
         imgs = []
         for name, data_chunk in data_chunks.items():
-            imgs.append(data_chunk.get('color')['value'])
+            imgs.append(data_chunk.get("color")["value"])
 
         # Apply the model
         results = self.model(imgs)
@@ -126,12 +135,18 @@ class YOLONode(cp.Node):
         data_chunk = cp.DataChunk()
 
         for i, name in enumerate(data_chunks):
+            data_chunk.add(f"xyx-{i}", results.pandas().xyxy[i])
+            data_chunk.add(f"render-{i}", renders[i], "image")
 
-            data_chunk.add(f'xyx-{i}', results.pandas().xyxy[i])
-            data_chunk.add(f'render-{i}', renders[i], 'image')
+        im_list_2d = []
+        for i in range(len(renders), 0, -1 * self.per_row_display):
+            im_list_2d.append(
+                [renders[i - (j + 1)] for j in range(self.per_row_display)]
+            )
 
-            cv2.imshow(f'{name}-{i}', imutils.resize(renders[i], width=400))
-            cv2.waitKey(1)
-        
+        im_tiles = self.concat_tiles(im_list_2d)
+        data_chunk.add("tiled", im_tiles, "image")
         return data_chunk
 
+    def concat_tiles(self, im_list_2d):
+        return cv2.vconcat([cv2.hconcat(im_list_h) for im_list_h in im_list_2d])
