352fa3c17f1cf99ee953bd8715ea7753c6fcd8bb
Umesh <umesh.timalsina@vanderbilt.edu>
2023-05-16 21:44:16 -0500

    WIP- #33: Add threshold bbox distance

--- mmlapipe/mf_sort_tracking/bbox_painter.py
+++ mmlapipe/mf_sort_tracking/bbox_painter.py
@@ -16,21 +16,30 @@ class BBoxPainter(cp.Node):
         self,
         frames_key: str = "frame",
         draw_boxes: bool = True,
+        put_text: bool = True,
         show: bool = False,
         video_title_prefix: Optional[str] = None,
         name: str = "BBoxPainter",
         paint_classes: Optional[List[int]] = None,
+        threshold_bbox_distance: Optional[int] = None,
         **kwargs,
     ) -> None:
         self.frames_key = frames_key
         self.draw_boxes = draw_boxes
+        self.put_text = put_text
         self.show = show
         self.video_title_prefix = video_title_prefix
         self.paint_classes = paint_classes
+        self.threshold_bbox_distance = threshold_bbox_distance
+        self.tracker_distances = None
         super().__init__(name=name, **kwargs)
 
+    def setup(self):
+        if self.threshold_bbox_distance:
+            self.tracker_distances = {}
+
     @staticmethod
-    def bbox_plot(
+    def _braw_boxes(
         img: np.ndarray,
         t: int,
         l: int,  # noqa: E741
@@ -45,7 +54,7 @@ class BBoxPainter(cp.Node):
         for bbox in frame.all_boxes:
             if bbox.cls in self.paint_classes:
                 t, l, w, h = bbox.tlwh.astype(int)  # noqa: E741
-                self.bbox_plot(
+                self._braw_boxes(
                     frame.arr, t, l, w, h, color=(0, 255, 0), thickness=-1
                 )
 
@@ -70,13 +79,50 @@ class BBoxPainter(cp.Node):
             for frame in frames:
                 img = frame.arr
                 for det in frame.detections:
+                    if self.threshold_bbox_distance:
+                        if det.tracker_id not in self.tracker_distances:
+                            self.tracker_distances[det.tracker_id] = []
+
+                        if len(self.tracker_distances[det.tracker_id]) == 1000:
+                            self.tracker_distances[det.tracker_id].pop(0)
+
+                        self.tracker_distances[det.tracker_id].append(
+                            det.bboxes[0].tlwh.astype(int)
+                        )
+
+                        if len(self.tracker_distances[det.tracker_id]) > 1:
+                            first_box = self.tracker_distances[det.tracker_id][
+                                0
+                            ]
+                            last_box = self.tracker_distances[det.tracker_id][
+                                -1
+                            ]
+                            ft, fl, fw, fh = first_box
+                            lt, ll, lw, lh = last_box
+                            fx1, fy1, fx2, fy2 = ft, fl, ft + fw, fl + fh
+                            lx1, ly1, lx2, ly2 = lt, ll, lt + lw, ll + lh
+                            distance = np.sqrt(
+                                (fx1 - lx1) ** 2 + (fy1 - ly1) ** 2
+                            )
+                            if distance > self.threshold_bbox_distance:
+                                self._braw_boxes(
+                                    img,
+                                    lt,
+                                    ll,
+                                    lw,
+                                    lh,
+                                    color=det.color,
+                                    thickness=2,
+                                )
+
                     for bbox in det.bboxes:
                         t, l, w, h = bbox.tlwh.astype(int)  # noqa: E741
 
                         if self.draw_boxes:
-                            self.bbox_plot(img, t, l, w, h, color=det.color)
+                            self._braw_boxes(img, t, l, w, h, color=det.color)
 
-                        self._put_text(img, t, l, det.get_text(), det.color)
+                        if self.put_text:
+                            self._put_text(img, t, l, det.get_text(), det.color)
 
                     if self.paint_classes is not None:
                         self._paint_classes(frame)
