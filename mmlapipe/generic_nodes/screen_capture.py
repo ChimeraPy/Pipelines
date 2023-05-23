f794cff572280ed55db1f3061c9227cacb629adf
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2023-05-23 14:12:14 -0500

    WIP- Documentation and Add mss as dependency

--- mmlapipe/generic_nodes/screen_capture.py
+++ mmlapipe/generic_nodes/screen_capture.py
@@ -1,12 +1,33 @@
+import typing
+
 import chimerapy as cp
 import cv2
 import imutils
 import numpy as np
 from chimerapy_orchestrator import source_node
 
+if typing.TYPE_CHECKING:
+    from mss.base import MSSBase
+
 
 @source_node(name="MMLAPIPE_ScreenCapture")
 class ScreenCapture(cp.Node):
+    """A generic video screen capture node using mss
+
+    Parameters
+    ----------
+    name : str, optional (default: 'ScreenCaptureNode')
+        The name of the node
+    scale: float, optional (default: 0.5)
+        The scale of the screen capture
+    fps: int, optional (default: 30)
+        The frame rate of the screen capture (unused, not guaranteed)
+    frame_key: str, optional (default: 'frame')
+        The key to use for the frame in the data chunk
+    monitor: int, optional (default: 0)
+        The monitor to capture
+    """
+
     def __init__(
         self,
         scale: float = 0.5,
@@ -25,7 +46,7 @@ class ScreenCapture(cp.Node):
     def setup(self):
         self.capture = None
 
-    def _get_capture(self):
+    def _get_capture(self) -> "MSSBase":
         import mss
 
         if self.capture is None:
@@ -40,11 +61,5 @@ class ScreenCapture(cp.Node):
 
         data_chunk = cp.DataChunk()
         data_chunk.add(self.frame_key, arr)
+
         return data_chunk
-
-
-if __name__ == "__main__":
-    node = ScreenCapture()
-    node.setup()
-    while True:
-        node.step()
